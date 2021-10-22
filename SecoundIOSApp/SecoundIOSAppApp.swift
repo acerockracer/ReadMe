aff6b48edfbdd6c8947aca97ce9a32d54722faa8
acerockracer <ace_rockracer@yahoo.com>
2021-10-22 08:29:17 -0500

    ReadMe

---
+++ SecoundIOSApp/SecoundIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecoundIOSAppApp.swift
+//  SecoundIOSApp
+//
+//  Created by AMStudent on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SecoundIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SecoundIOSApp/Views/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  SecoundIOSApp
+//
+//  Created by AMStudent on 9/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var library = Library()
+    
+    var body: some View {
+        
+        NavigationView {
+            List(library.sortedBooks) { book in
+            BookRow(
+                book: book,
+                    image: $library.uiImages[book]
+            )
+        }
+        .navigationBarTitle("My Library")
+        }
+}
+}
+struct BookRow: View {
+    @ObservedObject var book: Book
+    @Binding var image: UIImage?
+    var body: some View {
+        NavigationLink(
+            destination: DetailView(book: book, image: $image)
+            ) {
+        HStack {
+            Book.Image(
+                uiImage: image,
+                title: book.title,
+                size: 80,
+                cornerRadius:12
+            )
+            
+            VStack(alignment: .leading) {
+                TitleAndAuthorStack(
+                    book: book,
+                    titleFont: .title2,
+                    authorFont: .title3
+                )
+                
+                if !book.microReview.isEmpty{
+                Spacer()
+                Text(book.microReview)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                }
+            }
+            .lineLimit(1)
+            
+            Spacer()
+            
+            BookmarkButton(book: book)
+                .buttonStyle(BorderlessButtonStyle())
+        }
+        .padding(.vertical, 8)
+        }
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewedInAllColorSchemes
+    }
+}
